2bb67a3b43ce207918c5501df3e854d08cd38f19
Decpers Developement <71205293+decpers@users.noreply.github.com>
2020-10-03 12:42:05 -0500

    Add tags <head><body>

--- forms/request.php
+++ forms/request.php
@@ -1,11 +1,15 @@
 <html>
-	<div class="form" style="display: flex; margin-left: 40%;">
-		<form action="../students/students.php" method="POST">
-			<label>Write an id: </label><br>
-			<input type="number" name="id"><br><br>
-			<label>Write student's name: </label><br>
-			<input type="text" name="name"><br><br>
-			<input type="submit" name="" value="Save">
-		</form>
-	</div>
+	<head>
+	</head>
+	<body>
+		<div class="form" style="display: flex; margin-left: 40%;">
+			<form action="../students/students.php" method="POST">
+				<label>Write an id: </label><br>
+				<input type="number" name="id"><br><br>
+				<label>Write student's name: </label><br>
+				<input type="text" name="name"><br><br>
+				<input type="submit" name="" value="Save">
+			</form>
+		</div>
+	</body>
 </html>
